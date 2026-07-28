885b991993f5fe47e8d847a2ecf06fbf816c0430
abdullah18512 <abdvl.n18@gmail.com>
2026-07-28 16:33:51 +0500

    Task List designed

--- src/components/dashboard/EmployeeDashBoard.jsx
+++ src/components/dashboard/EmployeeDashBoard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../others/Header'
 import TaskListNumbers from '../others/TaskListNumbers'
+import TaskList from '../task_list/TaskList'
 
 const EmployeeDashBoard = () => {
   return (
@@ -8,6 +9,7 @@ const EmployeeDashBoard = () => {
       <div className='p-10 bg-[#1c1c1c] h-screen '>
         <Header/>
         <TaskListNumbers/>
+        <TaskList/>
       </div>
     </div>
   )
--- src/components/task_list/TaskList.jsx
+++ src/components/task_list/TaskList.jsx
@@ -2,8 +2,58 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div>
-      
+    <div id='tasklist' className='overflow-x-auto h-[55%] w-full py-5 flex items-center gap-4 justify-start mt-10 '>
+      <div className='shrink-0 h-full w-75 bg-emerald-200  rounded-2xl'>
+        <div className='flex justify-between m-3'>
+          <h3 className='bg-red-500 font-semibold px-3 rounded-2xl'>High</h3>
+          <h4 className='font-semibold'>Date</h4>
+        </div>
+        <div className='m-5'>
+          <h2 className='font-bold text-2xl'>Task</h2>
+          <span className='font-semibold '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, totam quaerat. Ipsa voluptatem sint saepe ipsam asperiores iste sit, pariatur doloremque inventore iusto dicta! Mollitia beatae labore debitis magni.
+          </span>
+        </div>
+      </div>
+
+      <div className='shrink-0 h-full w-75 bg-amber-200  rounded-2xl'>
+        <div className='flex justify-between m-3'>
+          <h3 className='bg-red-500 font-semibold px-3 rounded-2xl'>High</h3>
+          <h4 className='font-semibold'>Date</h4>
+        </div>
+        <div className='m-5'>
+          <h2 className='font-bold text-2xl'>Task</h2>
+          <span className='font-semibold '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, totam quaerat. Ipsa voluptatem sint saepe ipsam asperiores iste sit, pariatur doloremque inventore iusto dicta! Mollitia beatae labore debitis magni.
+          </span>
+        </div>
+      </div>
+
+      <div className='shrink-0 h-full w-75 bg-green-200 rounded-2xl'>
+        <div className='flex justify-between m-3'>
+          <h3 className='bg-red-500 font-semibold px-3 rounded-2xl'>High</h3>
+          <h4 className='font-semibold'>Date</h4>
+        </div>
+        <div className='m-5'>
+          <h2 className='font-bold text-2xl'>Task</h2>
+          <span className='font-semibold '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, totam quaerat. Ipsa voluptatem sint saepe ipsam asperiores iste sit, pariatur doloremque inventore iusto dicta! Mollitia beatae labore debitis magni.
+          </span>
+        </div>
+      </div>
+
+      <div className='shrink-0 h-full w-75 bg-rose-300  rounded-2xl'>
+        <div className='flex justify-between m-3'>
+          <h3 className='bg-red-500 font-semibold px-3 rounded-2xl'>High</h3>
+          <h4 className='font-semibold'>Date</h4>
+        </div>
+        <div className='m-5'>
+          <h2 className='font-bold text-2xl'>Task</h2>
+          <span className='font-semibold '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, totam quaerat. Ipsa voluptatem sint saepe ipsam asperiores iste sit, pariatur doloremque inventore iusto dicta! Mollitia beatae labore debitis magni.
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
