335301f1cff9fa535011a51cb14750a97fcb0441
abdullah18512 <abdvl.n18@gmail.com>
2026-07-29 16:44:35 +0500

    IDK

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 // import Login from './components/Auth/Login'
 import EmployeeDashBoard from './components/dashboard/EmployeeDashBoard'
+import AdminDashboard from './components/dashboard/AdminDashboard'
 
 const App = () => {
   return (
     <>
       {/* <Login /> */}
-      <EmployeeDashBoard />
+      {/* <EmployeeDashBoard /> */}
+      <AdminDashboard/>
     </>
 
   )
---
+++ src/components/dashboard/AdminDashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../others/Header'
+import { Card } from '../ui/card'
+import CreateList from '@/other_components/CreateList'
+
+const AdminDashboard = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-10 h-screen'>
+      <Header />
+      
+    </div>
+  )
+}
+
+export default AdminDashboard
